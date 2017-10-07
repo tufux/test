acc113fc9e0ccb7a6e2e2cfce8454f8b55915305
Kristian Eidem <tufu_x@hotmail.com>
2017-10-07 02:10:13 +0000

    assignment 3

---
+++ index.php
@@ -0,0 +1,7 @@
+<html>
+    <a href="/homework/homework1/index.html">homework1</a><br>
+    <a href="/homework/homework2/index.php">homework2</a><br>
+    <a href="/homework/homework3/index.php">homework3</a>
+    <a href="/labs/lab1/index.html">lab1</a><br>
+    <a href="/labs/lab2/index.php?score=0">lab2</a>
+</html>

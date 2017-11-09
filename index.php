638add3772b9735da2eb2f85854b04e83c22c529
Kristian Eidem <tufu_x@hotmail.com>
2017-11-09 04:42:41 +0000

    sigh....

--- index.php
+++ index.php
@@ -5,4 +5,5 @@
     <a href="/labs/lab1/index.html">lab1</a><br>
     <a href="/labs/lab2/index.php?score=0">lab2</a><br>
     <a href="/labs/lab5/index.php">lab5</a><br>
+    <a href="/labs/lab6/index.php">lab5</a><br>
 </html>
